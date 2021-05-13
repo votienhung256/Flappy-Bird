53c2a29345ec9c03db7c52174aa3809bc549f4ee
hung <hung.vt.62cntt4@ntu.edu.vn>
2021-05-13 19:41:59 +0700

    6

--- bird.js
+++ bird.js
@@ -1,15 +1,20 @@
 function Bird(x, y) {
     let _x = x
     let _y = y
+    let fall = function () {
+        _y += 3
+    }
+
+    setInterval(fall, 1000 / 30)
+
     this.getX = function () {
         return _x
     }
+
     this.getY = function () {
         return _y
     }
-    this.fall = function () {
-        _y += 3
-    }
+
     this.fly = function () {
         _y -= 60
     }
--- flappy_bird.js
+++ flappy_bird.js
@@ -13,9 +13,6 @@ let bird = new Bird(0,0)
 function run() {
     context.drawImage(hinhNen, 0, 0)
     context.drawImage(birdImg, bird.getX(), bird.getY())
-
-    
-    bird.fall()
     requestAnimationFrame(run)
 }
 
