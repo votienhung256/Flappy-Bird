cade0eab4920f40305ac56aa72d75d02f993a5a8
hung <hung.vt.62cntt4@ntu.edu.vn>
2021-05-18 19:23:00 +0700

    xoa comment

--- viewEngine.js
+++ viewEngine.js
@@ -16,30 +16,9 @@ function ViewEngine(game) {
 
         context.drawImage(bird.getImage(), bird.getX(), bird.getY())
 
-        // for (let i = 0; i < game.getPipe().pipes.length; i++) {
-        //     context.drawImage(game.getPipe().getImagePipeUp(), game.getPipe().pipes[i].x, game.getPipe().pipes[i].y)
-        //     context.drawImage(game.getPipe().getImagePipeUp(), game.getPipe().pipes[i].x, game.getPipe().pipes[i].y + game.getPipe().khoangCachDenOngduoi())
-
-        //     game.getPipe().pipes[i].x -= 5
-
-        //     if (game.getPipe().pipes[i].x == 144) {
-        //         game.getPipe().pipes.push({
-        //             x: 288,
-        //             y: Math.floor(Math.random() * 242) - 242
-        //         })
-        //     }
-        //     if (game.getPipe().pipes[i].x == 0) {
-        //         game.getPipe().pipes.splice(0, 1)
-        //     }
-        // }
-
-        //ve pipe 
         context.drawImage(warp.getTopPipeImg(), warp.getX(), warp.getTopPipeYPosition())
         context.drawImage(warp.getBotPipeImg(), warp.getX(), warp.getTopPipeYPosition() + warp.getHeight() + warp.getSize())
         
-        // ve pipe co chieu cao tuy y
-        // ve pipe co toa do tuy y
-        // ong lien tuc chay lui
         requestAnimationFrame(drawFrame)
     }
 }
